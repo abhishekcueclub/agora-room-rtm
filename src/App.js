abf08ef9cbd77115ed17de0afde7d6511b2699e0
Abhishek Sharma <tafzeel@joincueapp.com>
2022-12-12 16:18:42 +0530

    Added user listr

--- src/App.js
+++ src/App.js
@@ -73,13 +73,23 @@ export default function App() {
   }
 
   return (
-    <div className="call">
+    <div>
+      {remoteUsers.map((user) => (
+        <div key={user.uid}>
 
-      {/* <Permissions /> */}
-      <Draggable>
+          <label>{user.uid} </label> {" || "}
+          <label> {user._video_muted_ ? "Video disabled" : "Video enabled"}</label>
+          {" || "}
+          <label> {user._audio_muted_ ? "Audio disabled" : "Audio enabled"}</label>
+
+        </div>
+      ))}
+      <div className="call">
+
+        {/* <Draggable> */}
 
         <div className="local-player-wrapper" style={{
-          position: 'absolute'
+          position: 'absolute',
         }} >
           <MediaPlayer
             isSelf={true}
@@ -132,109 +142,109 @@ export default function App() {
         </div>
 
 
-      </Draggable>
-      {joinState ? <Launcher
-        agentProfile={{
-          teamName: 'Room Chat',
-          imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
-        }}
-        onMessageWasSent={(data) => {
-          // if (text.trim().length === 0) return;
-          console.log("event.target.value", data?.data?.text)
-          sendChannelMessage(data?.data?.text);
-          // setTextArea("");
-        }}
-        messageList={messages}
-        showEmoji
-      /> : null}
+        {/* </Draggable> */}
+        {joinState ? <Launcher
+          agentProfile={{
+            teamName: 'Room Chat',
+            imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
+          }}
+          onMessageWasSent={(data) => {
+            // if (text.trim().length === 0) return;
+            console.log("event.target.value", data?.data?.text)
+            sendChannelMessage(data?.data?.text);
+            // setTextArea("");
+          }}
+          messageList={messages}
+          showEmoji
+        /> : null}
 
-      {joinState ? <form className="call-form">
+        {joinState ? <form className="call-form">
 
-
-
-        <div className="button-group">
-          <br />
 
 
           <div className="button-group">
-            <input
-              type="text"
-              value={poketoUser}
-              name="poketoUser"
-              onChange={(event) => {
-                setPokeToUser(event.target.value);
-              }}
-            />
-            <button
-              type="button"
-              className="btn btn-primary btn-sm"
-              onClick={() => {
-                sendChannelMessageToPeer(poketoUser);
-              }}
-            >
-              Poke User
-            </button>
-            <br /><br />
-            <button
-              type="button"
-              className="btn btn-primary btn-sm"
-              onClick={() => {
-                pressedBuzzer(username);
-              }}
-            >
-              Press Buzzer
-            </button>
-            {"        "}
-            <button
-              id="clearBuzzerPressed"
-              type="button"
-              className="btn btn-primary btn-sm"
-              onClick={() => {
-                clearPressedBuzzer();
-              }}
-            >
-              Clear Buzzer
-
-            </button>
-
-            <br />
             <br />
 
 
-            <label>Who poked you {poked}</label>
-            <br />
-            <br />
-            <label>Buzzer is pressed by</label>
+            <div className="button-group">
+              <input
+                type="text"
+                value={poketoUser}
+                name="poketoUser"
+                onChange={(event) => {
+                  setPokeToUser(event.target.value);
+                }}
+              />
+              <button
+                type="button"
+                className="btn btn-primary btn-sm"
+                onClick={() => {
+                  sendChannelMessageToPeer(poketoUser);
+                }}
+              >
+                Poke User
+              </button>
+              <br /><br />
+              <button
+                type="button"
+                className="btn btn-primary btn-sm"
+                onClick={() => {
+                  pressedBuzzer(username);
+                }}
+              >
+                Press Buzzer
+              </button>
+              {"        "}
+              <button
+                id="clearBuzzerPressed"
+                type="button"
+                className="btn btn-primary btn-sm"
+                onClick={() => {
+                  clearPressedBuzzer();
+                }}
+              >
+                Clear Buzzer
 
-            <br />
-            <br />
-            {buzzersList?.map((user, index) => (
-              <div className="remote-player-wrapper" key={user}>
-                <label>{index + 1}: {user}</label>
-              </div>))}
+              </button>
 
+              <br />
+              <br />
+
+
+              <label>Who poked you {poked}</label>
+              <br />
+              <br />
+              <label>Buzzer is pressed by</label>
+
+              <br />
+              <br />
+              {buzzersList?.map((user, index) => (
+                <div className="remote-player-wrapper" key={user}>
+                  <label>{index + 1}: {user}</label>
+                </div>))}
+
+            </div>
           </div>
-        </div>
-      </form> : null}
+        </form> : null}
 
-      {
-        !joinState ? <form className="call-form">
+        {
+          !joinState ? <form className="call-form">
 
-          <label>
-            Username:
-            <input
-              value={username}
-              type="text"
-              name="username"
-              onChange={(event) => {
-                updateUsername(event.target.value);
-              }}
-            />
-          </label>
+            <label>
+              Username:
+              <input
+                value={username}
+                type="text"
+                name="username"
+                onChange={(event) => {
+                  updateUsername(event.target.value);
+                }}
+              />
+            </label>
 
-          <br />
-          <br />
-          {/* <label>
+            <br />
+            <br />
+            {/* <label>
             AppID:
             <input
               type="text"
@@ -249,104 +259,101 @@ export default function App() {
 
 
 
-          <label>
-            Token(Optional):
-            <input
-              type="text"
-              name="token"
-              value={token}
-              onChange={(event) => {
-                setToken(event.target.value);
-              }}
-            />
-          </label>
-          <label>
-            <b> Channel: </b>
-            <input
-              type="text"
-              value={channel}
-              name="channel"
-              onChange={(event) => {
-                setChannel(event.target.value);
-              }}
-            />
-          </label>
-
-          <div className="button-group">
-            <button
-              id="join"
-              type="button"
-              className="btn btn-primary btn-sm"
-              disabled={joinState}
-              onClick={() => {
-                join(channel, token, initRm, username);
-              }}
-            >
-              Join
-            </button>
-          </div>
-        </form> :
-          <form className="call-form">
-            <label>Currently Speaking: {currentSpeaker}</label>
-
-
+            <label>
+              Token(Optional):
+              <input
+                type="text"
+                name="token"
+                value={token}
+                onChange={(event) => {
+                  setToken(event.target.value);
+                }}
+              />
+            </label>
+            <label>
+              <b> Channel: </b>
+              <input
+                type="text"
+                value={channel}
+                name="channel"
+                onChange={(event) => {
+                  setChannel(event.target.value);
+                }}
+              />
+            </label>
 
             <div className="button-group">
               <button
-                id="leave"
+                id="join"
                 type="button"
                 className="btn btn-primary btn-sm"
-                disabled={!joinState}
+                disabled={joinState}
                 onClick={() => {
-                  leave();
+                  join(channel, token, initRm, username);
                 }}
               >
-                Leave
+                Join
               </button>
-              <br />
             </div>
-          </form>
-      }
+          </form> :
+            <form className="call-form">
+              <label>Currently Speaking: {currentSpeaker}</label>
 
-      {/* <b> */}
-      {/* {JSON.stringify(joinState) === "joined"
+
+
+              <div className="button-group">
+                <button
+                  id="leave"
+                  type="button"
+                  className="btn btn-primary btn-sm"
+                  disabled={!joinState}
+                  onClick={() => {
+                    leave();
+                  }}
+                >
+                  Leave
+                </button>
+                <br />
+              </div>
+            </form>
+        }
+
+        {/* <b> */}
+        {/* {JSON.stringify(joinState) === "joined"
           ? "No One is Here"
           : "Welcome to the '" + channel + "' Room"}
       </b>{" "} */}
-      <br />
-      {/* <b> Local Video: {JSON.stringify(localVideoTrack)}</b> <br />
+        <br />
+        {/* <b> Local Video: {JSON.stringify(localVideoTrack)}</b> <br />
       <b> Remote Video: {JSON.stringify(rmoteUsers)}</b> */}
-      <br />
-      <div className="player-container">
+        <br />
+        <div className="player-container">
 
 
-        <div className="remotePlayers">
-          {remoteUsers.map((user) => (
-            <div className="remote-player-wrapper" key={user.uid}>
-              {/* <p>Remote Player + {user.uid} </p> */}
-              {/* //   <p className="remote-player-text">{`remoteVideo(${user.uid})`}</p> */}
-              <MediaPlayer
-                isSelf={false}
-                videoTrack={user.videoTrack}
-                audioTrack={user.audioTrack}
-              ></MediaPlayer>
-              <label>{user.uid}</label>
-              <br />
-              <br />
-              <label>Video Enabled: {user._video_muted_ ? "false" : "true"}</label>
-              <br />
-
-              <label>Audio Enabled: {user._audio_muted_ ? "false" : "true"}</label>
+          <div className="remotePlayers">
+            {remoteUsers.map((user) => (
+              <div className="remote-player-wrapper" key={user.uid}>
+                {/* <p>Remote Player + {user.uid} </p> */}
+                {/* //   <p className="remote-player-text">{`remoteVideo(${user.uid})`}</p> */}
+                <MediaPlayer
+                  isSelf={false}
+                  videoTrack={user.videoTrack}
+                  audioTrack={user.audioTrack}
+                ></MediaPlayer>
+                <label>{user.uid} </label> {" || "}
+                <label> {user._video_muted_ ? "Video disabled" : "Video enabled"}</label>
+                {" || "}
+                <label> {user._audio_muted_ ? "Audio disabled" : "Audio enabled"}</label>
 
 
-              {/* <label>{JSON.stringify(user)}</label> */}
+                {/* <label>{JSON.stringify(user)}</label> */}
 
-            </div>
-          ))}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
 
-      {/* <div className="d-flex flex-column py-5 px-3">
+        {/* <div className="d-flex flex-column py-5 px-3">
         <h2>{channel} </h2>
 
         {messages.map((data, index) => {
@@ -360,7 +367,7 @@ export default function App() {
           );
         })}
       </div> */}
-      {/* <div>
+        {/* <div>
         <h2> {JSON.stringify(color)} </h2>
         <input
           placeholder="Type your message here"
@@ -370,6 +377,7 @@ export default function App() {
         />
       </div> */}
 
-    </div >
+      </div >
+    </div>
   );
 }
